30f0f918d5ac39f58822acb010840c1eab6bc25f
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2026-05-17 19:20:26 +0400

    Fixes

--- python-client/code/main.py
+++ python-client/code/main.py
@@ -16,20 +16,23 @@ CONFIG = {
     'database': 'marketdata'
 }
 
-NUM_QOUTES = 1000
+NUM_QUOTES = 1000
 NUM_TRADES = 100
 NS_IN_SEC  = 1000000000
-NS_IN_MS   = 1000000
 
-@trace('Connecting to database')
+
+@trace('Connecting to server')
 def connect():
    client = clickhouse_connect.get_client(**CONFIG)
-   print(f"Connected to a database {client.query('SELECT currentDatabase()').result_rows[0][0]}", flush=True)
+   if client.query('SELECT currentDatabase()').result_rows[0][0] != CONFIG['database']:
+      raise Exception(f"Database {CONFIG['database']} should be created during ClickHouse container startup")
    return client
+
 
 @trace('Dropping all tables')
 def reset_database(client):
    client.query(f'TRUNCATE DATABASE {CONFIG['database']}')
+
 
 @trace('Truncating data')
 def truncate_data(client):
@@ -37,17 +40,18 @@ def truncate_data(client):
    print("\n", flush=True)
    for table in tables:
       table_name = table[0]
-      print(f"Truncating table {table_name}", flush=True)
       client.command(f'TRUNCATE TABLE {table_name}')
+
 
 @trace('Creating schema')
 def init_schema(client, sc_scripts):
    for script in sc_scripts:
       client.command(script)
 
+
 # Alternatively load scripts from given folder in case we can't modify SQL scripts
 # Function assumes scripts should be applied in alphabetical order
-@trace('')
+@trace('Creating schema from files')
 def init_schema_from_script_files(client, scripts_path):
     path = Path(scripts_path)
     sc_scripts = sorted([str(f.resolve()) for f in path.glob('*_sc_*')])
@@ -55,11 +59,14 @@ def init_schema_from_script_files(client, scripts_path):
         script = Path(script_name).read_text(encoding='utf-8')
         client.command(script)
 
+
 def gen_quotes(quotes_total, chunk_size):
    time_ns = time.time_ns()
+   rows_inserted = 0
    for n in range(0, quotes_total, chunk_size):
       quotes = []
       for m in range(n, n+chunk_size):
+         rows_inserted += 1
          quotes.append(
             (random.randint(1, 100),  # bid_qty
              random.randint(1, 100),  # ask_qty
@@ -73,12 +80,17 @@ def gen_quotes(quotes_total, chunk_size):
       # next quote comes within random interval from 1 ns to 1 sec
       time_ns = time_ns + random.randint(1, NS_IN_SEC)
       yield quotes
+   if rows_inserted != quotes_total:
+      raise Exception(f'Wrong number of rows inserted into trades. Expected {quotes_total}, got {rows_inserted}')
+
 
 def gen_trades(trades_total, chunk_size):
    time_ns = time.time_ns()
+   rows_inserted = 0
    for n in range(0, trades_total, chunk_size):
       trades = []
       for m in range(n, n+chunk_size):
+         rows_inserted += 1
          trades.append(
             (random.randint(1, 100),  # qty
              random.randint(0, 2),    # side
@@ -92,15 +104,15 @@ def gen_trades(trades_total, chunk_size):
       # TODO: make sure trades and quotes are distributed across the same time window
       time_ns = time_ns + random.randint(1, NS_IN_SEC * 100)
       yield trades
+   if rows_inserted != trades_total:
+      raise Exception(f'Wrong number of rows inserted into trades. Expected {trades_total}, got {rows_inserted}')
 
 
 @trace('Inserting data')
 @avg_time(n_calls=1, verbose=True)
 def init_data(client, chunk_size=1):
-
-    for chunk in gen_quotes(NUM_QOUTES, chunk_size):
+    for chunk in gen_quotes(NUM_QUOTES, chunk_size):
        client.insert(table='md_quotes', data=chunk, column_names=['bid_qty', 'ask_qty', 'bid_price', 'ask_price', 'local_ts', 'exch_ts', 'symbol', 'source', 'seqno'])
-
     for chunk in gen_trades(NUM_TRADES, chunk_size):
        client.insert(table='md_trades', data=chunk, column_names=['qty', 'side', 'price', 'local_ts', 'exch_ts', 'symbol', 'source', 'seqno'])
 
@@ -109,19 +121,19 @@ def init_data(client, chunk_size=1):
 def check_data(client, verbose=False):
     if verbose:
        quotes = client.query("""
-         select bid_qty, ask_qty, bid_price, ask_price, toString(local_ts), symbol from md_quotes order by local_ts asc limit 5 union all
-         select bid_qty, ask_qty, bid_price, ask_price, toString(local_ts), symbol from md_quotes order by local_ts desc limit 5""")
+         select bid_qty, ask_qty, bid_price, ask_price, toString(local_ts) as local_ts, symbol from md_quotes order by local_ts asc limit 5 union all
+         select bid_qty, ask_qty, bid_price, ask_price, toString(local_ts) as local_ts, symbol from md_quotes order by local_ts desc limit 5""")
        print('\nQuotes')
        print_clickhouse_rowset(quotes, 10)
        trades = client.query("""
-         select qty, side, price, toString(local_ts), symbol from md_trades order by local_ts asc limit 5 union all
-         select qty, side, price, toString(local_ts), symbol from md_trades order by local_ts desc limit 5""")
+         select qty, side, price, toString(local_ts) as local_ts, symbol from md_trades order by local_ts asc limit 5 union all
+         select qty, side, price, toString(local_ts) as local_ts, symbol from md_trades order by local_ts desc limit 5""")
        print('\nTrades')
        print_clickhouse_rowset(trades, 10)
 
     quotes_row_count = client.query('SELECT COUNT(*) FROM md_quotes')
     trades_row_count = client.query('SELECT COUNT(*) FROM md_trades')
-    if quotes_row_count.result_rows[0][0] != NUM_QOUTES or trades_row_count.result_rows[0][0] != NUM_TRADES:
+    if quotes_row_count.result_rows[0][0] != NUM_QUOTES or trades_row_count.result_rows[0][0] != NUM_TRADES:
        raise Exception('Wrong number of quotes or trades')
 
 
@@ -147,8 +159,10 @@ def main():
    except Exception as e:
       print(f'\n\nException occurred in main(): {e}\n', flush=True)
       traceback.print_exc()
+      return -1
    finally:
       if client: client.close()
+   return 0
 
 if __name__ == '__main__':
    sys.exit(main())
