d9e5f72f14451e74a7efee4bda6f785fe363a4ba
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2026-05-18 15:19:19 +0400

    Fixes

--- python-client/code/sql.py
+++ python-client/code/sql.py
@@ -70,5 +70,5 @@ q_simple_asof_join = """
      ASOF LEFT JOIN md_quotes AS q
          ON t.symbol = q.symbol 
          AND t.local_ts >= q.local_ts  
-     ORDER BY t.local_ts
+     ORDER BY spread_to_bid desc
 """
