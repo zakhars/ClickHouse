47667cd9ba51711ee72f167c73833c1bef6272d8
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2026-05-18 15:05:28 +0400

    Added repeatable join

--- python-client/code/main.py
+++ python-client/code/main.py
@@ -231,7 +231,7 @@ def get_physical_size(client):
 
 
 @trace('Joining')
-@avg_time(n_calls=1)
+@avg_time(n_calls=100)
 def join_simple(client, rows_to_print=-1):
    joined = client.query(sql.q_simple_asof_join)
    print(f'\nNumber of rows returned by JOIN: {joined.row_count}. Top {rows_to_print} rows:')
