f22b87b37c9018b0c4846fc8e52bb39416dcd823
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2026-05-19 10:18:09 +0400

    Fixes

--- python-client/code/sql.py
+++ python-client/code/sql.py
@@ -39,10 +39,29 @@ sc_create_table_md_trades = """
       index_granularity = 1024;
 """
 
-sc_create_table_md_trades_memory = """
-   CREATE TABLE IF NOT EXISTS trades_in_memory
-   ENGINE = Join(ANY, LEFT, symbol, local_ts)
-   AS SELECT * FROM md_trades;
+sc_materized_view = """
+   CREATE MATERIALIZED VIEW mv_trade_quote_asof_join
+   ENGINE = MergeTree
+   ORDER BY (symbol, trade_local_ts)
+   SETTINGS index_granularity = 1024
+   POPULATE  -- Fill immediately
+   AS
+   SELECT 
+         t.symbol,
+         t.side as trade_side,
+         t.qty as trade_qty,
+         t.local_ts as trade_local_ts,
+         q.local_ts as quote_local_ts,
+         round(t.price, 5) as trade_price,
+         round(q.bid_price, 5) as bid_price,
+         round(q.ask_price, 5) as ask_price,
+         round(t.price - q.bid_price, 5) as spread_to_bid,
+         round(q.ask_price - t.price, 5) as spread_to_ask
+         dateDiff('microsecond', q.local_ts, t.local_ts) AS quote_to_request_latency_us
+   FROM md_trades AS t
+   ASOF LEFT JOIN md_quotes AS q
+       ON t.symbol = q.symbol 
+       AND t.local_ts >= q.local_ts
 """
 
 q_simple_asof_join = """
@@ -58,11 +77,18 @@ q_simple_asof_join = """
          round(q.ask_price, 5) as ask_price,
          round(t.price - q.bid_price, 5) as spread_to_bid,
          round(q.ask_price - t.price, 5) as spread_to_ask
+--         dateDiff('microsecond', q.local_ts, t.local_ts) AS quote_to_request_latency_us
      FROM md_trades AS t
      ASOF LEFT JOIN md_quotes AS q
          ON t.symbol = q.symbol 
          AND t.local_ts >= q.local_ts
 --     WHERE t.local_ts between '2026-04-27 00:00:00' AND '2026-04-27 23:59:59.999'   
-     PREWHERE t.local_ts between '2026-04-27 00:00:00' AND '2026-04-27 23:59:59.999'
+--     PREWHERE t.local_ts between '2026-04-27 00:00:00' AND '2026-04-27 23:59:59.999'
 """
 
+
+q_select_from_mv = """
+   SELECT * 
+   FROM trade_quote_asof_mv
+   WHERE t.local_ts between '2026-04-27 00:00:00' AND '2026-04-27 23:59:59.999' 
+"""
