908c6c7fd1b0a88d5ed662cdf358e4948c494805
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2026-05-19 10:03:46 +0400

    Fixes

--- python-client/code/main.py
+++ python-client/code/main.py
@@ -235,10 +235,10 @@ def get_physical_size(client):
 @avg_time(n_calls=100, verbose=True)
 def join_simple(client, rows_to_print=-1):
    joined = client.query(sql.q_simple_asof_join
-      ,settings=
-      {
-         #'join_algorithm': 'full_sorting_merge'
-      }
+      # ,settings=
+      # {
+      #    'join_algorithm': 'full_sorting_merge'
+      # }
       )
    print(f'\nNumber of rows returned by JOIN: {joined.row_count}', flush=True)
    #print_clickhouse_rowset(joined, rows_to_print)
