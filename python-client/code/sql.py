d3437d4611c3334a138fd3c0b165a07b4c26ea63
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2026-05-20 20:03:24 +0400

    Partition removed

--- python-client/code/sql.py
+++ python-client/code/sql.py
@@ -91,8 +91,8 @@ q_asof_join = """
      ASOF LEFT JOIN md_quotes AS q
          ON t.symbol = q.symbol 
          AND t.local_ts >= q.local_ts
-     WHERE 
---     PREWHERE 
+--     WHERE 
+     PREWHERE 
    q.local_ts between '2026-04-27 00:00:00' AND '2026-04-27 23:59:59.999'
 --     AND 
 --     symbol = 'F.EPZ26'
