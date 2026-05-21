ea132a732d443f405f25725ed3b2a1b5213a634c
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2026-05-22 03:16:48 +0400

    Partition removed

--- python-client/code/main.py
+++ python-client/code/main.py
@@ -328,14 +328,24 @@ def main():
          context.filter = filter
          run_test(client, context, False, 'Join  FILTER')
 
-      # Compare INDEX GRANULARITY
-      print_test_header('Compare INDEX GRANULARITY')
+      # Compare INDEX GRANULARITY for hash
+      print_test_header('Compare INDEX GRANULARITY for hash algorithm')
       config.REGENERATE_DATASET = True
       context.join_settings = config.JOIN_SETTINGS['hash']
       context.filter = config.FILTER['none']
       for granularity in config.INDEX_GRANULARITY.values():
          context.granularity = granularity
          run_test(client, context, False, 'Table INDEX GRANULARITY')
+
+      # Compare INDEX GRANULARITY for full_sorting_merge
+      print_test_header('Compare INDEX GRANULARITY for full_sorting_merge algorithm')
+      config.REGENERATE_DATASET = True
+      context.join_settings = config.JOIN_SETTINGS['full_sorting_merge']
+      context.filter = config.FILTER['none']
+      for granularity in config.INDEX_GRANULARITY.values():
+         context.granularity = granularity
+         run_test(client, context, False, 'Table INDEX GRANULARITY')
+
 
       print_test_header('Selecting from MV')
       select_from_mv(client, rows_to_print=-1)
